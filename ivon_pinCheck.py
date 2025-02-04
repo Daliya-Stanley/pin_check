bfe2685c6e0b3d7d025103130a44f8754221d935
Ivon Martinez <ivon.mrtnz@gmail.com>
2025-02-04 18:39:21 +0000

    Created new python file ivon_pinCheck.py

---
+++ ivon_pinCheck.py
@@ -0,0 +1,18 @@
+import getpass
+
+supplied_pin = None
+myPin = 1234
+tries = 3
+count = 0
+
+while count < tries:
+    supplied_pin = getpass.getpass(prompt="Enter your PIN: " , stream=None)
+    if int(supplied_pin) == myPin:
+        print("Correct PIN")
+        break
+    else:
+        count+=1
+        if tries - count == 0:
+            print("Wrong PIN! No more attempts")
+            break
+        print("Wrong PIN!" + " You have " + str(tries - count) + " more attempts")
